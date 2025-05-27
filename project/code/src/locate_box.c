314ad5c08f37ac2f28d222be2fd1a36e761d0f4e
Cancer <1761721506@qq.com>
2025-05-27 21:55:32 +0800

    修改改了定位逻辑，尝试了矫正

--- project/code/src/locate_box.c
+++ project/code/src/locate_box.c
@@ -1,6 +1,6 @@
 #include "locate_box.h"
 
-#define AREA_CON_REF	100//面积控制期望
+#define AREA_CON_REF	180//面积控制期望
 #define X_CON_REF		0//x坐标控制期望
 
 rt_sem_t locate_box_sem;
@@ -28,19 +28,57 @@ void locate_test(){
 
 	while(1){
 		if(mt9v03x_finish_flag){
-
+			Camera_PreProcess();
 			Camera_FindMidLine();
+			
+			Vision_Draw();
+
 			//获取中线
 			for(int i=imgRow-1;i>=0;i--)
 				Image_S.MID_Table[i]=(int16)((Image_S.rightBroder[i]+Image_S.leftBroder[i])/2);
 
-			//截取部分中线的平均值（主要是远处部分）
-			int aver_error;
-			for(int i = 0; i<=69-20;i++)
-				aver_error += (imgCol/2 - Image_S.MID_Table[i])/imgRow*2;
+			int16 MID_Table[imgRow];
+			int16 leftBroder[imgRow];//左边边界
+			int16 rightBroder[imgRow];//右边边界
+			for(int i=imgRow-1;i>=0;i--){
+				MID_Table[i] = Image_S.MID_Table[i];
+				leftBroder[i] = Image_S.leftBroder[i];
+				rightBroder[i] = Image_S.rightBroder[i];
+			}
+
+			for(int i=imgRow-1;i>0;i--)
+			{
+				if(MID_Table[i]>=188)
+					MID_Table[i] = 187;
+					MID_Table[i] = MID_Table[i]<0? 0:MID_Table[i];
+
+				if(leftBroder[i]>=188)
+					leftBroder[i] = 187;
+					leftBroder[i] = leftBroder[i]<0? 0:leftBroder[i];
+
+				if(rightBroder[i]>=188)
+					rightBroder[i] = 187;
+					rightBroder[i] = rightBroder[i]<0? 0:rightBroder[i]; 
+							
+				ips200_draw_point(MID_Table[i], i, RGB565_RED);
+				ips200_draw_point(leftBroder[i], i, RGB565_BLUE);
+				ips200_draw_point(rightBroder[i], i, RGB565_BROWN);
+				//中线
+				ips200_draw_point((int)(94), i, RGB565_GREEN);
+			}
+
+
+
+			//截取部分中线的平均值?
+			float aver_error;
+			float error;
+			for(int i = 69-10; i<=69;i++)
+				error = error + (imgCol/2 - Image_S.MID_Table[i])/10.0f;
 
 			//控制环路
-			Car_Change_Speed(Pos_PID_Controller(&locate_box_data.Transverse_pid,aver_error),0,Pos_PID_Controller(&locate_box_data.Dir_Cen_pid,center_x));
+			aver_error = error;
+			Car_Change_Speed(Pos_PID_Controller(&locate_box_data.Dir_pid,aver_error),0,Pos_PID_Controller(&locate_box_data.Dir_Cen_pid,center_x));
+			error = 0;
 
 			mt9v03x_finish_flag = 0;
 		}
@@ -85,7 +123,7 @@ void direction_correction_test(){
 				break;
 
 			case Location_Correct_State:
-				Car_Change_Speed(180,Pos_PID_Controller(&locate_box_data.Longitudinal_pid,center_y),Pos_PID_Controller(&locate_box_data.Dir_Cen_pid,center_x));
+				Car_Change_Speed(200,Pos_PID_Controller(&locate_box_data.Longitudinal_pid,center_y),Pos_PID_Controller(&locate_box_data.Dir_Cen_pid,center_x));
 				if(fabs(Att_CurrentYaw - init_angle)>=90){
 					angle_state = Push_Box_State;
 					Car_Change_Speed(0,0,0);
@@ -96,19 +134,19 @@ void direction_correction_test(){
 
 			case Push_Box_State:
 				Car_Change_Speed(Pos_PID_Controller(&locate_box_data.Transverse_pid,center_x),Pos_PID_Controller(&locate_box_data.Longitudinal_pid,center_y),0);
-				if(abs(center_x - X_CON_REF)<=4){
-					Car_Change_Speed(0,0,0);
-					rt_thread_delay(100);
-					Car_DistanceMotion(0,60,1.5);
-					Car_DistanceMotion(0,-30,1.5);
+				// if(abs(center_x - X_CON_REF)<=4){
+				// 	Car_Change_Speed(0,0,0);
+				// 	rt_thread_delay(100);
+				// 	Car_DistanceMotion(0,60,1.5);
+				// 	Car_DistanceMotion(0,-30,1.5);
 
-					Car_Change_Yaw(init_angle);
-					rt_thread_delay(500);
-					MCX_Change_Mode(MCX_Detection_Mode);
-					Car_Speed_ConRight = Con_By_TraceLine;
-					finish_flag = 1;
+				// 	Car_Change_Yaw(init_angle);
+				// 	rt_thread_delay(500);
+				// 	MCX_Change_Mode(MCX_Detection_Mode);
+				// 	Car_Speed_ConRight = Con_By_TraceLine;
+				// 	finish_flag = 1;
 
-				}
+				// }
 				break;
 			
 			default:
@@ -133,7 +171,8 @@ void direction_correction_test(){
  */
 void locate_box_debug(){
 	
-	direction_correction_test();
+	//direction_correction_test();
+	locate_test();
 	
 }
 
@@ -174,32 +213,32 @@ void locate_box_init()
 		while(1);
 	}
 
-	locate_box_thread = rt_thread_create("locate",locate_box_entry,RT_NULL,1024,3,1000);
+	locate_box_thread = rt_thread_create("locate",locate_box_entry,RT_NULL,4096,2,1000);
 	rt_thread_startup(locate_box_thread);
 	
 	//纵向PID初始化
-	Pos_PID_Init(&locate_box_data.Longitudinal_pid,1,0,0);
+	Pos_PID_Init(&locate_box_data.Longitudinal_pid,1.2,0,0.5);
 	locate_box_data.Longitudinal_pid.Output_Max = 500;
 	locate_box_data.Longitudinal_pid.Output_Min = -500;
 	locate_box_data.Longitudinal_pid.Value_I_Max = 500;
 	locate_box_data.Longitudinal_pid.Ref = AREA_CON_REF;
 	
 	//横向PID初始化
-	Pos_PID_Init(&locate_box_data.Transverse_pid,1,0,0);
+	Pos_PID_Init(&locate_box_data.Transverse_pid,1.2,0,0.5);
 	locate_box_data.Transverse_pid.Output_Max = 500;
 	locate_box_data.Transverse_pid.Output_Min = -500;
 	locate_box_data.Transverse_pid.Value_I_Max = 500;
 	locate_box_data.Transverse_pid.Ref = X_CON_REF;
 
-	//一次矫正PID初始化
-	Pos_PID_Init(&locate_box_data.Dir_Cen_pid,-1,0,0);
+	//一次矫正PID初始化-1.3,0,-0.5
+	Pos_PID_Init(&locate_box_data.Dir_Cen_pid,-0.8,0,0);
 	locate_box_data.Dir_Cen_pid.Output_Max = 500;
 	locate_box_data.Dir_Cen_pid.Output_Min = -500;
 	locate_box_data.Dir_Cen_pid.Value_I_Max = 500;
 	locate_box_data.Dir_Cen_pid.Ref = X_CON_REF;
 
 	//二次矫正PID初始化
-	Pos_PID_Init(&locate_box_data.Dir_pid,-1.3,0,0);
+	Pos_PID_Init(&locate_box_data.Dir_pid,2,0,0);
 	locate_box_data.Dir_pid.Output_Max = 500;
 	locate_box_data.Dir_pid.Output_Min = -500;
 	locate_box_data.Dir_pid.Value_I_Max = 500;
